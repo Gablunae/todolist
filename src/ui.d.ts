91a52c2bf7570e01724d3a5d8ffdd005a2c660d0
Yuly <rubertonegabii@gmail.com>
2025-10-06 21:09:25 -0300

    borré cosas

--- src/ui.d.ts
+++
@@ -1,5 +0,0 @@
-import { Tarea } from './tarea';
-export declare function mostrarMenuPrincipal(prompt: any): string;
-export declare function mostrarMenuVerTareas(prompt: any): string;
-export declare function mostrarListadoTareas(prompt: any, lista: Tarea[], tareas: Tarea[]): void;
-//# sourceMappingURL=ui.d.ts.map
